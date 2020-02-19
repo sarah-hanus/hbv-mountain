67c813627dd4a0fbb23f938499dfb40e4123b3d1
Sarah Hanus <sarah.hanus@posteo.de>
2020-02-19 17:11:16 +0100

    Add NSElog to objective functions

--- ObjectiveFunctions.jl
+++ ObjectiveFunctions.jl
@@ -1,11 +1,36 @@
 module ObjectiveFunctions
-export NSE
-function NSE(Qobserved, Qmodelled)
+export nse
+export lognse
+
+# """
+#     NSE(Qobserved, Qmodelled)
+#
+# Compute the Nash Sutclifee efficiency for a model using the observed discharge ('Qobserved')
+# and the modelled discharge ('Qmodeled')
+# """
+
+function nse(Qobserved, Qmodelled)
     QobservedAverage = sum(Qobserved) / length(Qobserved) #float
-    QobservedAverage = ones(length(Qobserved))*QobservedAverage
+    QobservedAverage = ones(length(Qobserved)) * QobservedAverage
     Nominator = sum((Qobserved-Qmodelled).^2)
     Denominator = sum((Qobserved - QobservedAverage).^2)
     NashSutcliffe = 1 - (Nominator / Denominator)
     return NashSutcliffe
+end
+
+# """
+#     NSElog(Qobserved, Qmodelled)
+#
+# Compute the logarithmic Nash Sutclifee efficiency for a model using the observed discharge ('Qobserved')
+# and the modelled discharge ('Qmodeled')
+# """
+
+function lognse(Qobserved, Qmodelled)
+    QobservedAverage = sum(Qobserved) / length(Qobserved) #float
+    QobservedAverage = ones(length(Qobserved)) * QobservedAverage # average as array
+    Nominator = sum((log.(Qobserved)-log.(Qmodelled)).^2)
+    Denominator = sum((log.(Qobserved) - log.(QobservedAverage)).^2)
+    NashSutcliffelog = 1 - (Nominator / Denominator)
+    return NashSutcliffelog
 end
 end
