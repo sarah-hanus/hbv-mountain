688e9b3c9e09d8286b11b6555c87a8394f0c10a7
Sarah Hanus <sarah.hanus@posteo.de>
2020-03-25 18:58:44 +0100

    Tests to test whats wrong with the soil reservoir

--- src/Test/test_soil.jl
+++ src/Test/test_soil.jl
@@ -127,3 +127,30 @@ using Test
 
     end
 end
+
+@testset "soilstorage_run" begin
+    for i in 1:100
+        #parameters that don't change
+        beta = rand(0.5:0.1:4)[1]
+        Ce =  rand(0.4:0.01:0.8)[1]
+        Percolationcapacity = rand(0.1:0.1:4)[1]
+        Ratio_Pref = rand(0.1:0.1:0.9)[1]
+        Soilstoragecapacity = rand(50:5:400)[1]
+
+        #parameters that change
+        Effective_Precipitation = rand(0.1:0.5:15)[1]
+        Interception_Evaporation = rand(0.1:0.1:4)[1]
+        Potential_Evaporation = rand(0.1:0.2:10)[1]
+        Storage = rand(1:5:Soilstoragecapacity)[1]
+
+        # if no precipitation and storage zero, all outputs are zero
+        Storage = 0
+        for i in 1:1000
+            Effective_Precipitation = rand(0.1:0.5:15)[1]
+            Interception_Evaporation = rand(0.1:0.1:4)[1]
+            Potential_Evaporation = rand(0.1:0.2:10)[1]
+            Overlandflow, Percolationflow, Preferentialflow, Soil_Evaporation, Soilstorage = soilstorage(Effective_Precipitation, Interception_Evaporation, Potential_Evaporation, Storage, beta, Ce, Percolationcapacity, Ratio_Pref, Soilstoragecapacity)
+            Storage = Soilstorage
+            @test Storage <= Soilstoragecapacity
+    end
+end
