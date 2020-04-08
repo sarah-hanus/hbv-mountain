cef134f8bb45f806f8d947cf58d961f62f2a372d
Sarah Hanus <sarah.hanus@posteo.de>
2020-04-08 15:59:36 +0200

    + Changed NSE and NSElog, so that mean is directly calculated with mean function + Added functions for Flow Duration Curves, Autocorrelation Value, Autocorrelation Curve, Runoff Ratio, Snow Cover + Functions have been run with real data but not yet tested with tests

--- src/ObjectiveFunctions.jl
+++ src/ObjectiveFunctions.jl
@@ -1,38 +1,192 @@
-function nse(Qobserved, Qmodelled)
+function nse(Qobserved::Array{Float64, 1}, Qmodelled::Array{Float64, 1})
     # input is array of modelled and observed data
-    QobservedAverage = sum(Qobserved) / length(Qobserved) #float
-    QobservedAverage = ones(length(Qobserved)) * QobservedAverage
+    #QobservedAverage = sum(Qobserved) / length(Qobserved) #float
+    QobservedAverage = ones(length(Qobserved)) * mean(Qobserved)
     Nominator = sum((Qobserved-Qmodelled).^2)
     Denominator = sum((Qobserved - QobservedAverage).^2)
     NashSutcliffe = 1 - (Nominator / Denominator)
-    return NashSutcliffe
+    return NashSutcliffe::Float64
 end
 
-function lognse(Qobserved, Qmodelled)
-    QobservedAverage = sum(Qobserved) / length(Qobserved) #float
-    QobservedAverage = ones(length(Qobserved)) * QobservedAverage # average as array
+function lognse(Qobserved::Array{Float64, 1}, Qmodelled::Array{Float64, 1})
+    #QobservedAverage = sum(Qobserved) / length(Qobserved) #float
+    QobservedAverage = ones(length(Qobserved)) * mean(Qobserved) # average as array
     Nominator = sum((log.(Qobserved)-log.(Qmodelled)).^2)
     Denominator = sum((log.(Qobserved) - log.(QobservedAverage)).^2)
     NashSutcliffelog = 1 - (Nominator / Denominator)
-    return NashSutcliffelog
+    return NashSutcliffelog::Float64
 end
 
-function volumetricefficiency(Qobserved, Qmodelled)
+function volumetricefficiency(Qobserved::Array{Float64, 1}, Qmodelled::Array{Float64, 1})
     Nominator = sum(abs.(Qmodelled - Qobserved))
     Denominator = sum(Qobserved)
     Ve = 1 - Nominator / Denominator
-    return Ve
+    return Ve::Float64
 end
 
-function flowdurationcurve(Q)
+function flowdurationcurve(Q::Array{Float64, 1})
     # input as array, the discharge has to be sorted from largest to smallest and assigned value
     # check function with data of former exercises
     SortedQ = sort(Q, rev = true)
     Rank = collect(1 : length(Q))
-    Exceedanceprobability = Rank ./ (SortedQ .+ 1)
+    #WRONG IT should be current rank/ (total number of events (Q) +1)
+    Exceedanceprobability = Rank ./ (length(SortedQ) .+ 1)
     # exceedence probability should not be higher than 1???
-    return SortedQ, Exceedanceprobability
+    return SortedQ::Array{Float64, 1}, Exceedanceprobability::Array{Float64, 1}
 end
+
+function autocorrelation(Q::Array{Float64, 1}, Timelag::Int64)
+    # the function shifts the normal discharge by a timelag of x days
+    # function from Euser et al 2013
+    Qshort = Q[1 : end - Timelag]
+    Sum_Nominator = 0
+    Sum_Denominator = 0
+    for (i, Current_Discharge) in enumerate(Qshort)
+        Nominator = (Current_Discharge - mean(Qshort)) * (Q[i + Timelag] - mean(Qshort))
+        Denominator = (Current_Discharge - mean(Qshort))^2
+        Sum_Nominator += Nominator
+        Sum_Denominator += Denominator
+    end
+    AC = Sum_Nominator / Sum_Denominator
+    return AC::Float64
+end
+
+function autocorrelation2(Q::Array{Float64, 1}, Timelag::Int64)
+    Qshifted = Q[1 + Timelag: end]
+    Q = Q[1 : end - Timelag]
+    Q_average = ones(length(Q)) * mean(Q)
+    Nominator = sum((Q -  Q_average) .* (Qshifted - Q_average))
+    Denominator = sum((Q - Q_average).^2)
+    AC = Nominator / Denominator
+    return AC::Float64
+end
+
+function autocorrelationcurve(Q::Array{Float64, 1}, Timelag::Int64)
+    # this function calculates an autocorrelation curve by shifting the discharge by a day until reaching the timelag
+    # than the autocorrelation value of each time lag is calculated and is plotted against days of the lag
+    # the first entry to evaluate is the entry after the timelag, so for example 31 if timelage=30
+    Q_normal = Q[Timelag + 1: end]
+    AC = Float64[]
+    for i in 0 : Timelag
+        Qshifted = Q[Timelag + 1 - i : end - i]
+        @assert length(Qshifted) == length(Q_normal)
+        Correlation = cor(Q_normal, Qshifted)
+        push!(AC, Correlation)
+    end
+    @assert length(AC) == Timelag + 1
+    Lags = collect(0: Timelag)
+    return AC::Array{Float64, 1}, Lags::Array{Int64, 1}
+
+
+end
+
+function autocorrelationcurve2(Q::Array{Float64, 1}, Timelag::Int64)
+    Q_normal = Q[Timelag + 1: end]
+    Q_average = ones(length(Q_normal)) * mean(Q_normal)
+    AC = Float64[]
+    for i in 0 : Timelag
+        Qshifted = Q[Timelag + 1 - i : end - i]
+        @assert length(Qshifted) == length(Q_normal)
+        Nominator = sum((Q_normal -  Q_average) .* (Qshifted - Q_average))
+        Denominator = sum((Q_normal - Q_average).^2)
+        Correlation = Nominator / Denominator
+        push!(AC, Correlation)
+    end
+    @assert length(AC) == Timelag + 1
+    Lags = collect(0: Timelag)
+    return AC::Array{Float64, 1}, Lags::Array{Int64, 1}
+end
+
+function monthlyrunoff(Area, Precipitation::Array{Float64, 1}, Discharge::Array{Float64, 1}, Timeseries::Vector{Any})
+    # function calculates the monthly runoff coefficient of each month in the timeseries
+    # discharge is given in m3/s and precipitation in mm/d
+    # convert Discharge to mm/d
+    Discharge = Discharge / Area * (1000 * 3600 * 24)
+    sum_Precipitation = 0
+    sum_Discharge = 0
+    monthly_Runoff = Float64[]
+    Month = String[]
+    for (i, current_date) in enumerate(Timeseries)
+        # the precipitation and discharges are summed up
+        sum_Precipitation += Precipitation[i]
+        sum_Discharge += Discharge[i]
+        current_date = Date(current_date, dateformat"y,m,d")
+        day = Dates.day(current_date)
+        # if the last day of the month is reached
+        if day == Dates.daysinmonth(current_date)
+            monthly_Runoff_Current = sum_Discharge / sum_Precipitation
+            push!(monthly_Runoff, monthly_Runoff_Current)
+            sum_Precipitation = 0
+            sum_Discharge = 0
+            Month_Current = Dates.format(current_date, "yyyy-mm")
+            push!(Month, Month_Current)
+        end
+    end
+    return monthly_Runoff, Month
+end
+
+function averagemonthlyrunoff(monthly_Runoff::Vector{Float64}, Month::Vector{String})
+    # calculates the average runoff coefficient of a certain month over the timeperiod
+    # all runoff coefficients of a certain months are summed up
+    # assert that
+    @assert length(monthly_Runoff) == length(Month)
+    sum_January = 0
+    sum_February = 0
+    sum_March = 0
+    sum_April = 0
+    sum_Mai = 0
+    sum_June = 0
+    sum_July = 0
+    sum_August = 0
+    sum_Sept = 0
+    sum_Okt = 0
+    sum_Nov = 0
+    sum_Dec = 0
+    for (i, current_date) in enumerate(Month)
+        current_date = Date(current_date, dateformat"yyyy-mm")
+        current_month = Dates.month(current_date)
+        if current_month == 1
+            sum_January += monthly_Runoff[i]
+        elseif current_month == 2
+            sum_February += monthly_Runoff[i]
+        elseif current_month == 3
+            sum_March += monthly_Runoff[i]
+        elseif current_month == 4
+            sum_April += monthly_Runoff[i]
+        elseif current_month == 5
+            sum_Mai += monthly_Runoff[i]
+        elseif current_month == 6
+            sum_June += monthly_Runoff[i]
+        elseif current_month == 7
+            sum_July += monthly_Runoff[i]
+        elseif current_month == 8
+            sum_August += monthly_Runoff[i]
+        elseif current_month == 9
+            sum_Sept += monthly_Runoff[i]
+        elseif current_month == 10
+            sum_Okt += monthly_Runoff[i]
+        elseif current_month == 11
+            sum_Nov += monthly_Runoff[i]
+        else
+            sum_Dec += monthly_Runoff[i]
+        end
+    end
+    sum_monthly_Runoff = [sum_January, sum_February, sum_March, sum_April, sum_Mai, sum_June, sum_July, sum_August, sum_Sept, sum_Okt, sum_Nov, sum_Dec]
+    FirstYear = Dates.year(Date(Month[1], dateformat"yyyy-mm"))
+    LastYear = Dates.year(Date(Month[end], dateformat"yyyy-mm"))
+    Number_Years = LastYear - FirstYear + 1
+    @assert Number_Years * 12 == length(monthly_Runoff)
+    average_monthly_Runoff = sum_monthly_Runoff / Number_Years
+    return average_monthly_Runoff::Vector{Float64}
+end
+
+
+function snowcover(Modeled_Area::Array{Float64, 1}, Observed_Area::Array{Float64, 1})
+    Difference = (1 - abs.(Modeled_Area - Observed_Area))
+    Mean_Difference = mean(Difference)
+    return Mean_Difference
+end
+
 
 export nse
 export lognse
