c90833312f2c9e425aa7d9bd6bd6e1e658b3b38a
Sarah Hanus <sarah.hanus@posteo.de>
2020-04-24 17:32:41 +0200

    + Changes test for slow storage a bit

--- src/Test/test_slowstorage.jl
+++ src/Test/test_slowstorage.jl
@@ -9,21 +9,21 @@ prec = 15
         AreaRiparian = rand(0:0.01:1)[1]
         #Preferential = rand(0:0.1:10)[1]
         # test that output 0, if input 0
-        @test slowstorage(0, 0, Ks, Ratio_Rip) == (0, 0, 0)
+        @test slowstorage(0., 0.,AreaRiparian, Ks, Ratio_Rip) == (0, 0, 0)
         # test that storage decreases if no input
-        Riparian_Discharge, Slow_Discharge, Slowstorage = slowstorage(0, Storage, AreaRiparian, Ks, 0.)
-        @test round(Storage - Slowstorage, digits=prec) == round(Slow_Discharge, digits=prec)
-        Riparian_Discharge, Slow_Discharge, Slowstorage = slowstorage(0, Storage, AreaRiparian, 0., Ratio_Rip)
+        Riparian_Discharge, Slow_Discharge, Slowstorage = slowstorage(0., Storage, AreaRiparian, Ks, 0.)
+        @test -eps(Float64)*10 <= Storage - Slowstorage - Slow_Discharge <= eps(Float64)*10
+        Riparian_Discharge, Slow_Discharge, Slowstorage = slowstorage(0., Storage, AreaRiparian, 0., Ratio_Rip)
         @test round(Storage - Slowstorage, digits=prec) == round(Riparian_Discharge, digits=prec)
         # test that discharge should be the Ks-ratio of sum of storage and overlandflow
         Riparian_Discharge, Slow_Discharge, Slowstorage = slowstorage(GWflow, Storage, AreaRiparian, Ks, Ratio_Rip)
-        @test round((Storage + GWflow) * Ks, digits=prec)  == round(Slow_Discharge + Riparian_Discharge, digits = prec)
+        @test round((Storage + GWflow) * Ks, digits=prec)  == round(Slow_Discharge + Riparian_Discharge / AreaRiparian, digits = prec)
         @test round(Storage + GWflow - Slow_Discharge - Riparian_Discharge, digits = prec) == round(Slowstorage, digits = prec)
         # test right behavior if storage = 0
         Riparian_Discharge, Slow_Discharge, Slowstorage = slowstorage(GWflow, 0, AreaRiparian, Ks, Ratio_Rip)
         @test GWflow * Ks * (1 - Ratio_Rip) == Slow_Discharge
-        @test round(GWflow * Ks, digits=prec) == round(Slow_Discharge + Riparian_Discharge, digits=prec)
+        @test round(GWflow * Ks, digits=prec) == round(Slow_Discharge + Riparian_Discharge / AreaRiparian, digits=prec)
         # if storage empty before, input = output+ new storage
-        @test round(Slow_Discharge + Riparian_Discharge + Slowstorage, digits=prec) == round(GWflow, digits=prec)
+        @test -eps(Float64)*10 <= Slow_Discharge + Riparian_Discharge + Slowstorage - GWflow <= eps(Float64)*10
     end
 end
